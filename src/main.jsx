3dd62eb9b0424bbdb7cddae65b8465ad1c72da2d
tandat0303 <ls17189a3.11@gmail.com>
2026-01-14 16:23:31 +0700

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { ConvexProvider, ConvexReactClient } from 'convex/react'
+
+const convex = new ConvexReactClient(import.meta.env.VITE.CONVEX.URL)
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <ConvexProvider client={convex}>
+        <App />
+      </ConvexProvider>
+    </ClerkProvider>
+  </StrictMode>,
+)
